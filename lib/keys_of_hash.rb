85d0dfb6e4a333c52b9406c09d985e5251c73f26
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-10 20:55:57 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,9 +1,10 @@
 class Hash
   def keys_of(hash, *arguments)
     array = []
-    puts hash
     puts arguments
     hash.each do |key, value|
+      puts key
+      puts value
       if value == arguments
         array << key
       end
